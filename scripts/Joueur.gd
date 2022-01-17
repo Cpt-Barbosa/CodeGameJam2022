b31cf348df49b55f7be600dd016d09f6ce1ca31d
Cpt-Barbosa <noa.barbosa@hotmail.fr>
2022-01-17 18:14:03 +0100

    Changement mouvement joueur

--- scripts/Joueur.gd
+++ scripts/Joueur.gd
@@ -1,35 +1,21 @@
 extends KinematicBody2D
-#Position variables used for movement
-var x = 100
-var y = 0
-#Angle variable used for movement
-var deg = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.get_child(1).play()
+export (int) var speed = 200
 
-#Handles input via keyboard, mouse or controller
-func _input(event):
-	if(event.is_action("move_up")):
-		x=0
-		y=-100
-		deg=-90
-	elif(event.is_action("move_down")):
-		x=0
-		y=100
-		deg=90
-	elif(event.is_action("move_right")):
-		x=100
-		y=0
-		deg=0
-	elif(event.is_action("move_left")):
-		x=-100
-		y=0
-		deg=-180
-	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	#if Input.is_action_pressed("move_down"):
+	#	velocity.y += 1
+	#if Input.is_action_pressed("move_up"):
+	#	velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
 func _physics_process(delta):
-	self.move_and_collide(Vector2(x*delta,y*delta))
-	self.set_rotation(deg2rad(deg))
+	get_input()
+	velocity = move_and_slide(velocity)
