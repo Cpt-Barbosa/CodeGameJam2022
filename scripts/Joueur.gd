eeda2b72c6af966766c9d00b7bcd4e76903bb530
Cpt-Barbosa <noa.barbosa@hotmail.fr>
2022-01-17 17:54:40 +0100

    Ajout action et scene joueur

--- scripts/Joueur.gd
+++ scripts/Joueur.gd
@@ -1,16 +1,35 @@
 extends KinematicBody2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+#Position variables used for movement
+var x = 100
+var y = 0
+#Angle variable used for movement
+var deg = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.get_child(1).play()
 
-
+#Handles input via keyboard, mouse or controller
+func _input(event):
+	if(event.is_action("move_up")):
+		x=0
+		y=-100
+		deg=-90
+	elif(event.is_action("move_down")):
+		x=0
+		y=100
+		deg=90
+	elif(event.is_action("move_right")):
+		x=100
+		y=0
+		deg=0
+	elif(event.is_action("move_left")):
+		x=-100
+		y=0
+		deg=-180
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	self.move_and_collide(Vector2(x*delta,y*delta))
+	self.set_rotation(deg2rad(deg))
