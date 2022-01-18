9e29b69188ce37bea94983320073abf02f6679cf
Cpt-Barbosa <noa.barbosa@hotmail.fr>
2022-01-18 09:45:50 +0100

    Ajout sprite coffre et saut basique

--- scripts/Joueur.gd
+++ scripts/Joueur.gd
@@ -1,34 +1,41 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 350
 var God_Mod = true
 const FLOOR_NORMAL = Vector2(0 , -1)
+var jump_count = 2
+#...
+# Vertical impulse applied to the character upon jumping in meters per second.
+export var jump_impulse = 9000
 
 var velocity = Vector2()
 
-func _ready():
-	self.set_scale(Vector2( 1, 1 ))
 func get_input():
 	velocity = Vector2()
+	if !self.is_on_floor():
+			self.get_child(1).set_animation("falling")
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x += speed
 		if self.is_on_floor():
-			self.get_child(1).set_frame(0)
+			self.get_child(1).set_animation("moving")
+			self.get_child(1).set_flip_h(false)
 	elif Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		velocity.x -= speed
 		if self.is_on_floor():
-			self.get_child(1).set_frame(1)
-	else :
-		if !self.is_on_floor():
-			self.get_child(1).set_frame(2)
-	velocity = velocity.normalized() * speed
+			self.get_child(1).set_animation("moving")
+			self.get_child(1).set_flip_h(true)
+	if Input.is_action_just_pressed("jump"):
+		if jump_count > 0:
+			jump_count -= 1
+			velocity.y -= jump_impulse	
+	velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity")
 	#print(str(position.y))
 
 func _physics_process(delta):
 	get_input()
-	velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity")
 	velocity = move_and_slide(velocity,FLOOR_NORMAL)
-	
+	if self.is_on_floor():
+		jump_count=2
 	if self.get_child(3).GetBarreVide() && !God_Mod:
 		print("mort")
 
