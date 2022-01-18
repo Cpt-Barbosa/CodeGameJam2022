ec8d44cba8b0f2bd5349f7fd0116076b4e73cefd
Similisi <johannfouchard25@gmail.com>
2022-01-18 11:54:39 +0100

    ajout bg et maj oxy

--- scripts/barreOxy.gd
+++ scripts/barreOxy.gd
@@ -3,12 +3,13 @@ extends CanvasLayer
 var TailleMax = self.transform.x
 var temps = 0
 var Est_Vide = false
+export var delay = 0.05
 
 func _physics_process(delta):
 	self.get_child(0).transform.x = TailleMax - (temps* TailleMax / 100)
 	
 	if self.get_child(0).transform.x.x > 0:
-		temps += 1#0.05
+		temps += delay
 	else:
 		BarreVide()
 	#print(str(self.transform.x))
